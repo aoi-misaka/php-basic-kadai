424005793f2723ea6785cc7e01da9230dfb89e8c
aoi-misaka <aoimisaka.05@gmail.com>
2025-04-11 15:34:39 +0900

    修正

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
    <meta charset="UTF-8">
@@ -7,11 +7,19 @@
 <body>
    <p>
        <?php 
-        $aryHoge = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+         $score1 = 80;
+         $score2 = 60;
+         $score3 = 55;
+         $score4 = 40;
+         $score5 = 100;
+         $score6 = 25;
+         $score7 = 80;
+         $score8 = 95;
+         $score9 = 30;
+         $score10 =60;
 
-        $fuga = array_sum($aryHoge) / count($aryHoge);
+         echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) /10 ;
 
-        echo $fuga;
         ?>
-
+   </p>
 </html>
